00ed23bca093e14463dc8cd1be0d4d2c02bb8061
ashley <ashley.oketo@student.moringaschool.com>
2022-08-17 17:41:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+
+const employee = {
+    name : "Ashley",
+    streetAddress : "1234"
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+  
+  const newEmployee = { ...employee };
+
+  newEmployee[key] = value;
+
+  return newEmployee;
+
+  
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+
+  employee[key] = value;
+
+  return employee;
+}
+function deleteFromEmployeeByKey(employee,key,value){
+
+  const fromEmployee = { ... employee };
+
+  fromEmployee[key] = value;
+
+  delete fromEmployee.key;
+
+  return fromEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key,value){
+  employee[key] = value;
+
+  delete employee.key;
+
+  return employee;
+}
